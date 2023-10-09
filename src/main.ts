7e5e83d9fc37b88665b20285688ffd446d0013a7
jonathanforhan <jonathan.forhan@gmail.com>
2023-10-09 02:43:24 -0400

    move all styles to css

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,6 @@ import * as textureUrl from "/public/snhu.png";
 const canvas = document.querySelector("#gl-canvas")! as HTMLCanvasElement;
 const matrix = document.querySelector<MathfieldElement>('#matrix')!;
 
-document.querySelector("body")!.style.margin = "0";
-document.querySelector("body")!.style.padding = "0";
-document.querySelector("body")!.style.overflow = "clip";
-
 /* Main Driver */
 (async function main() {
   const renderer = new Renderer(canvas, matrix);
